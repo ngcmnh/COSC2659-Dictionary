8f2e5f80165a5205a2aad96d0a95ceb6ffde5335
ngcmnh <s3907086@rmit.edu.vn>
2023-09-09 09:54:38 +0700

    first commit

---
+++ Dictionary23am/Dictionary23am/Dictionary23amApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary23amApp.swift
+//  Dictionary23am
+//
+//  Created by ngminh on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dictionary23amApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
