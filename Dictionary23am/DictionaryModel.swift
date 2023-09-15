166aa78db35fdf1cfce3f855d9c978d75b745393
AnhDoanNep <140772322+AnhDoanNep@users.noreply.github.com>
2023-09-15 13:34:51 +0700

    Dictionary Search

---
+++ Dictionary23am/DictionaryModel.swift
@@ -0,0 +1,43 @@
+//
+//
+//  DictionaryModel.swift
+//  //
+//  Created by Đoàn Hoàng Anh on 12/09/2023.
+//
+
+import Foundation
+
+struct DictionaryEntry: Codable {
+    let word: String
+    let phonetics: [Phonetic]
+    let meanings: [Meaning]
+    let license: License
+    let sourceUrls: [String]
+}
+
+struct Phonetic: Codable {
+    let audio: String?
+    let sourceUrl: String?
+    let license: License?
+    let text: String?
+}
+
+struct License: Codable {
+    let name: String
+    let url: String
+}
+
+struct Meaning: Codable {
+    let partOfSpeech: String
+    let definitions: [Definition]
+    let synonyms: [String]?
+    let antonyms: [String]?
+}
+
+struct Definition: Codable {
+    let definition: String
+    let synonyms: [String]?
+    let antonyms: [String]?
+    let example: String?
+}
+
---
+++ Dictionary23am/DictionaryViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  DictionaryViewModel.swift
+//  
+//
+//  Created by Đoàn Hoàng Anh on 12/09/2023.
+//
+
+import Foundation
+import AVFoundation
+
+enum Category: Int {
+    case shortDescription
+    case fullDescription
+    case pronounce
+    case synonym
+    case antonym
+}
+enum SearchState {
+    case none, searched, cleared
+}
+
+class DictionaryViewModel: ObservableObject {
+    
+    @Published var entry: DictionaryEntry?
+    @Published var word: String = ""
+    @Published var selectedSegment = 0
+    @Published var selectedCategory = Category.shortDescription
+    @Published var searchState: SearchState = .none
+    var player: AVPlayer?
+    
+    func playAudio(from url: String) {
+        guard let audioURL = URL(string: url) else { return }
+        player = AVPlayer(url: audioURL)
+        player?.play()
+    }
+    
+    func fetchDefinition() {
+        guard let url = URL(string: "https://api.dictionaryapi.dev/api/v2/entries/en_US/\(word)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            
+            do {
+                let entries = try JSONDecoder().decode([DictionaryEntry].self, from: data)
+                DispatchQueue.main.async {
+                    self.entry = entries.first
+                }
+            } catch {
+                print("Failed to parse dictionary entry: \(error)")
+                self.entry = nil
+                self.searchState = .searched
+            }
+        }
+        task.resume()
+    }
+}
